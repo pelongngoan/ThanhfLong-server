3c7fd7c870ae82dadc38935ef96f11a98ccc32a6
thanhflong <long.tl446@gmail.com>
2025-07-04 14:14:47 +0700

    [BE] Config Mongoose connection and add Account schema

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
-const app = express();
-const port = process.env.PORT;
 
+dotenv.config();
+
+import { connectDB } from "./config/mongoDb.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI || "";
+
+connectDB(MONGO_URI);
 app.get("/", (req, res) => {
-  res.send("Hello from TypeScript Expressd!");
+  res.send("Hello from ThanhfLong!");
 });
 
 app.listen(port, () => {
